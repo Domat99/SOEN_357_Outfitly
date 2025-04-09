01b003cfd32b8185d6fe24eb26386f86107d5e30
RawdaWaez <rawdawaez@gmail.com>
2025-04-09 18:59:34 -0400

    enhancement

--- app/src/App.js
+++ app/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './Homepage/HomePage';
 import Navbar from './Navbar/Navbar';
@@ -7,20 +7,22 @@ import ProfilePage from './Profile/ProfilePage';
 import LoginPage from './Login/LoginPage';
 import OutfitPlanner from './Planner/OutfitPlanner';
 import WeatherPage from './Weather/WeatherPage';
+import {initialClosetItems} from "./Closet/initialClosetItems";
 
 
 
 function App() {
-    console.log("Rendering App.js...");
+    const [closetItems, setClosetItems] = useState(initialClosetItems);
+
     return (
         <Router>
             <Navbar isLoggedIn={false} />
             <Routes>
                 <Route path="/" element={<HomePage />} />
-                <Route path="/closet" element={<Closet />} />
+                <Route path="/closet" element={<Closet closetItems={closetItems} setClosetItems={setClosetItems} />} />
                 <Route path="/profile" element={<ProfilePage />} />
                 <Route path="/login" element={<LoginPage />} />
-                <Route path="/planner" element={<OutfitPlanner />} />
+                <Route path="/planner" element={<OutfitPlanner closetItems={closetItems} />} />
                 <Route path="/weather" element={<WeatherPage />} />
             </Routes>
         </Router>
