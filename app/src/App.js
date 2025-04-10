70cba16d456bf25fba58b5cfbc3e6351b17806e6
RawdaWaez <rawdawaez@gmail.com>
2025-04-10 14:02:02 -0400

    Sign up sign in

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,11 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './Homepage/HomePage';
 import Navbar from './Navbar/Navbar';
 import Closet from './Closet/Closet';
 import ProfilePage from './Profile/ProfilePage';
 import LoginPage from './Login/LoginPage';
+import SignUp from './Login/SignUp';
 import OutfitPlanner from './Planner/OutfitPlanner';
 import WeatherPage from './Weather/WeatherPage';
 import {initialClosetItems} from "./Closet/initialClosetItems";
@@ -13,15 +14,29 @@ import {initialClosetItems} from "./Closet/initialClosetItems";
 
 function App() {
     const [closetItems, setClosetItems] = useState(initialClosetItems);
+    const [loggedInUser, setLoggedInUser] = useState(null);
+
+    useEffect(() => {
+        const storedUser = JSON.parse(localStorage.getItem('loggedInUser'));
+        if (storedUser) {
+            setLoggedInUser(storedUser);
+        }
+    }, []);
+
+    const handleLogout = () => {
+        localStorage.removeItem('loggedInUser');
+        setLoggedInUser(null);
+    };
 
     return (
         <Router>
-            <Navbar isLoggedIn={false} />
+            <Navbar loggedInUser={loggedInUser} handleLogout={handleLogout} />
             <Routes>
                 <Route path="/" element={<HomePage />} />
                 <Route path="/closet" element={<Closet closetItems={closetItems} setClosetItems={setClosetItems} />} />
                 <Route path="/profile" element={<ProfilePage />} />
-                <Route path="/login" element={<LoginPage />} />
+                <Route path="/login" element={<LoginPage setLoggedInUser={setLoggedInUser} />} />
+                <Route path="/Signup" element={<SignUp />} />
                 <Route path="/planner" element={<OutfitPlanner closetItems={closetItems} />} />
                 <Route path="/weather" element={<WeatherPage />} />
             </Routes>
